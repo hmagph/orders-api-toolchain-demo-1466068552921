0c3284b5e59fec3860b84a1acea53e00f05bdb8d
PHILIPPE MULET <philippe_mulet@fr.ibm.com>
2016-07-08 07:04:59 -0500

    register in service discovery

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/*globals cloudantService:true */
+/*globals cloudantService:true discovery:true*/
 /*eslint-env node */
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -25,4 +25,38 @@ app.post('/rest/orders', orders.create);
 
 app.listen(appEnv.port, appEnv.bind);
 console.log('App started on ' + appEnv.bind + ':' + appEnv.port);
+
+//Register in service discovery with heatbeat
+var sdcreds = appEnv.getService("myMicroservicesDiscovery").credentials;
+discovery = new ServiceDiscovery({
+  name: 'ServiceDiscovery',
+  auth_token: sdcreds.auth_token,
+  url: sdcreds.url,
+  version: 1
+});
+discovery.register(
+  {
+    service_name: appEnv.name,
+    ttl: 30, // 30s
+    endpoint: {
+      type: 'http',
+      value: appEnv.url
+    },
+    metadata: {}
+  },
+  function(err, response, body) {
+  if (!err && response.statusCode === 201) {
+    var id = body.id;
+    console.log('Registered', body);
+    setInterval(function() {
+      discovery.renew(id, function(err, response) {
+        if (!err && response.statusCode === 200) {
+          console.log('HEARTBEAT OK');
+        } else {
+          console.log('HEARTBEAT ERROR');
+        }
+      });
+    }, 3000);
+  }
+});
 
